643f33240c9d8adbed5ffa89584190fff98b7f9b
jriver27 <jrivera@uh.edu>
2015-02-05 16:53:41 -0600

    Created TEAM4OIES team project.

---
+++ TEAM4OIES/TEAM4OIES/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TEAM4OIES.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "Welcome to Team 4 Project MVC!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ TEAM4OIES/TEAM4OIES/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TEAM4OIES.MvcApplication" Language="C#" %>
