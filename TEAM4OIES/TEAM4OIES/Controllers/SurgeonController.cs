884818717005e755b68c85fb574d6034f2e9b76e
danieluh <uhdanielg@gmail.com>
2015-04-12 17:22:02 -0500

    basic surgeon

---
+++ TEAM4OIES/TEAM4OIES/Controllers/SurgeonController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TEAM4OIES.Controllers
+{
+    public class SurgeonController : Controller
+    {
+        //
+        // GET: /Surgeon/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
