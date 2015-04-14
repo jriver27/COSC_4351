8f9176ddd4c6b0ac0f1e034e10907c877c58bf76
danieluh <uhdanielg@gmail.com>
2015-04-13 20:06:14 -0500

    Revert "adding diccom cleaner folder"
    This reverts commit 9594c07fbf3371ddbbb92ac6e3fa7fe1d7bbf2c9.

--- TEAM4OIES/TEAM4OIES/bin/DICCOMcleaner/DicomCleaner.bat
+++
@@ -1,3 +0,0 @@
-path=.;.\lib\additional;%path%
-.\windows\jre7\bin\java -Xms256m -Xmx512m -cp ".\pixelmed.jar;.\lib\additional\hsqldb.jar;.\lib\additional\commons-compress-1.9.jar;.\lib\additional\vecmath1.2-1.14.jar;.\lib\additional\commons-codec-1.3.jar;.\lib\additional\jmdns.jar" com.pixelmed.display.DicomCleaner
-pause
