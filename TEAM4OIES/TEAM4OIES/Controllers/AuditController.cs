4f5b0707307096a51de4a3d36172525159042173
Paul Miller <Paulmiller347664@gmail.com>
2015-04-19 05:55:56 -0500

    started work on auditing

---
+++ TEAM4OIES/TEAM4OIES/Controllers/AuditController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TEAM4OIES.Controllers
+{
+    public class AuditController : Controller
+    {
+        //
+        // GET: /Audit/
+
+        public ActionResult Index()
+        {
+
+            TEAM4OIES.Models.AuditService audit = new TEAM4OIES.Models.AuditService();
+            audit.addtoAudit(4, "Paul", "audit", "cookie", "full");
+            return View();
+        }
+
+    }
+}
---
+++ TEAM4OIES/TEAM4OIES/Models/AuditService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.Windows.Forms;
+
+
+
+namespace TEAM4OIES.Models
+{
+   
+    public class AuditService 
+    {
+        public Boolean addtoAudit(int UserID, String username, String tablename, String attribute, String access)
+        {
+            DateTime date = DateTime.Now;
+            Console.WriteLine(date.ToString());
+            TEAM4OIESDataSet auditdataset= new TEAM4OIESDataSet();
+            TEAM4OIESDataSet.AuditRow newAuditRow = auditdataset.Audit.NewAuditRow();
+            newAuditRow.UserID = UserID;
+            newAuditRow.date = date;
+            newAuditRow.username = username;
+            newAuditRow.table_ = tablename;
+            newAuditRow.attribute = attribute;
+            newAuditRow.access = access;
+            auditdataset.Audit.Rows.Add(newAuditRow);
+            return true;
+        }
+    }
+   
+}
