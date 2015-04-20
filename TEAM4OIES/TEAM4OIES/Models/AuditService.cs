d362b023cc46cd72754c43663bc75001012cf552
Paul Miller <Paulmiller347664@gmail.com>
2015-04-20 15:15:45 -0500

    changed to dataset1

--- TEAM4OIES/TEAM4OIES/Models/AuditService.cs
+++ TEAM4OIES/TEAM4OIES/Models/AuditService.cs
@@ -19,16 +19,8 @@ namespace TEAM4OIES.Models
         public Boolean addtoAudit(int UserID, String username, String tablename, String attribute, String access)
         {
             DateTime date = DateTime.Now;
-            Console.WriteLine(date.ToString());
-            TEAM4OIESDataSet auditdataset= new TEAM4OIESDataSet();
-            TEAM4OIESDataSet.AuditRow newAuditRow = auditdataset.Audit.NewAuditRow();
-            newAuditRow.UserID = UserID;
-            newAuditRow.date = date;
-            newAuditRow.username = username;
-            newAuditRow.table_ = tablename;
-            newAuditRow.attribute = attribute;
-            newAuditRow.access = access;
-            auditdataset.Audit.Rows.Add(newAuditRow);
+            DataSet1TableAdapters.AuditTableAdapter auditRowAdapter = new DataSet1TableAdapters.AuditTableAdapter();
+            auditRowAdapter.InsertQuery(UserID, username, date, tablename, attribute, access);
             return true;
         }
     }
