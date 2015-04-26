8ad9fba3b326849bcb3a1da6630f188bbf8e4d32
lmtong <linhmtong@hotmail.com>
2015-04-26 07:16:34 -0500

    search and add testimonial UCs are done

--- TEAM4OIES/TEAM4OIES/Models/Testimonials.cs
+++ TEAM4OIES/TEAM4OIES/Models/Testimonials.cs
@@ -7,18 +7,10 @@ namespace TEAM4OIES.Models
 {
     public class Testimonials
     {
-        //public int testimonialID { private set; get; }
-        public String content { private set; get; }
-        public DateTime tDate{ private set; get; }
-        //public int surgeonID { private set; get; }
-        public String firstName { private set; get; }
-        public String lastName { private set; get; }
-        public Testimonials(String content_, DateTime tDate_, String firstName_, String lastName_)
-        {
-            firstName = firstName_;
-            content = content_;
-            tDate = tDate_;
-            lastName = lastName_;
-        }
+        public String content { set; get; }
+        public String tDate{ set; get; }
+        public String firstName { set; get; }
+        public String lastName { set; get; }  
     }
+
 }
