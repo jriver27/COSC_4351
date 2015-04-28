1ce9c08a108e99ff6c5e120fc2f354f7ac9039ad
Sarah Moore <SarahNinety@hotmail.com>
2015-04-27 20:16:31 -0500

    Extracted method and did Hilford comments

--- TEAM4OIES/TEAM4OIES/Controllers/SurgeonController.cs
+++ TEAM4OIES/TEAM4OIES/Controllers/SurgeonController.cs
@@ -27,25 +27,48 @@ namespace TEAM4OIES.Controllers
             if (file != null && file.ContentLength > 0)
             {
                 string extension = Path.GetExtension(file.FileName);
+                //make sure the extension isn't a zip
                 if (extension!=".zip")
                 {
-                    FileStream stream = System.IO.File.OpenRead(Path.GetFullPath(file.FileName));
-
-                    FileStream outFile = System.IO.File.Create(file.FileName + ".zip");
-                    
-                    GZipStream Compress = new GZipStream(outFile, CompressionMode.Compress);
-
+                    UC4ZipAnonymizedPatientData(file);
                 }
-                // extract only the fielname
-                var fileName = System.IO.Path.GetFileName(file.FileName);
+                else
+                {
+                    // extract only the fielname
+                    var fileName = System.IO.Path.GetFileName(file.FileName);
 
-                // store the file inside ~/App_Data/uploads folder
-                var path = System.IO.Path.Combine(@"D:/COSC4351_Spring2015/TEAM4OIES/"
-, fileName);
-                file.SaveAs(path);
+                    // store the file inside ~/App_Data/uploads folder
+                    var path = System.IO.Path.Combine(@"D:/COSC4351_Spring2015/TEAM4OIES/", fileName);
+                    file.SaveAs(path);
+                }
             }
             // redirect back to the index action to show the form once again
             return RedirectToAction("Index");
+        }
+        /*
+         * UC4ZipAnonymizedPatientData
+         * Sarah Moore
+         * 4/27/2015
+         * 
+         * */
+        private static void UC4ZipAnonymizedPatientData(HttpPostedFileBase file)
+        {
+            //while the file is open
+            using (FileStream stream = System.IO.File.OpenRead(Path.GetFullPath(file.FileName)))
+            {
+                //create the stream of the zip file name
+                FileStream outFile = System.IO.File.Create(file.FileName + ".zip");
+                //compress the outfile
+                GZipStream Compress = new GZipStream(outFile, CompressionMode.Compress);
+                //copy data from the original file to the compressed file
+                stream.CopyTo(Compress);
+                string endDirectory = @"D:/COSC4351_Spring2015/TEAM4OIES/";
+                //save the compressed file
+                using (FileStream destinationStream = System.IO.File.Create(endDirectory))
+                {
+                    stream.CopyTo(destinationStream);
+                }
+            }
         }
     }
 }
