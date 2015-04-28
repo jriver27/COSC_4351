cfeb258944d300ddfc71191a16f97938930712e3
jriver27 <jrivera@uh.edu>
2015-04-28 14:29:47 -0500

    Fixing UC13 into TEAM4OIES.csproj

--- TEAM4OIES/TEAM4OIES/Models/AuditService.cs
+++ TEAM4OIES/TEAM4OIES/Models/AuditService.cs
@@ -4,10 +4,6 @@ using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Globalization;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Security;
-using System.Windows.Forms;
 
 
 
